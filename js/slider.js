8eeee2064695df001d7a183ec416ddcce5f9d59f
unknown <sahu.himanshu2016@gmail.com>
2023-08-12 13:38:27 +0530

    updated

--- js/slider.js
+++ js/slider.js
@@ -21,5 +21,26 @@ $(document).ready(function() {
         }
    
     });
+    //instagram-feed
+    $("#instagram-feed").owlCarousel({
+        autoPlay: 3000, //Set AutoPlay to 3 seconds
+        // nav:true,
+        items : 6,
+        // itemsDesktop : [1199,3],
+        // itemsDesktopSmall : [979,3],
+        navText:["<div class='nav-btn prev-slide'></div>","<div class='nav-btn next-slide'></div>"],
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:6
+            }
+        }
+   
+    });
    
   });
